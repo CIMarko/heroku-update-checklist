d73a7490e9d0b35c02f7c7601e42d1c0373dde90
Anna Greaves <gilhespy.anna@gmail.com>
2022-11-21 11:42:08 +0100

    Create dropdowns for course and 4P course position.

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+/* Variables */
+const formWrapper = document.getElementById("formWrapper");
+const courseForm = document.getElementById("courseForm");
+const P4CoursePositionForm = document.getElementById("P4CoursePosition");
+
+/**
+ * Handles form to select which course the student is on
+ * @param {form event} event 
+ */
+function handleCourseForm(event) {
+    event.preventDefault();
+    let form = event.target;
+    let course = form.selectCourse.value;
+    console.log(course);
+}
+
+
+/**
+ * Handles form to select which section of the 4P course the student is on
+ * @param {form event} event 
+ */
+function handle4PCoursePositionForm(event) {
+    event.preventDefault();
+    let form = event.target;
+    let P4CoursePosition = form.selectP4CoursePosition.value;
+    console.log(P4CoursePosition);
+}
+
+/* Event listeners */
+courseForm.addEventListener("submit", handleCourseForm)
+P4CoursePositionForm.addEventListener("submit", handle4PCoursePositionForm)
