73695060fe5117f4aa15fed8bc22aa26925081fd
Anna Greaves <gilhespy.anna@gmail.com>
2022-11-21 11:50:59 +0100

    Add handleMS3DatabaseType dropdown.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 /* Variables */
-const formWrapper = document.getElementById("formWrapper");
 const courseForm = document.getElementById("courseForm");
 const P4CoursePositionForm = document.getElementById("P4CoursePosition");
+const MS3DatabaseType = document.getElementById("MS3DatabaseType");
 
 /**
  * Handles form to select which course the student is on
@@ -26,6 +26,19 @@ function handle4PCoursePositionForm(event) {
     console.log(P4CoursePosition);
 }
 
+
+/**
+ * Handles form to select which type of database was used for MS3
+ * @param {form event} event 
+ */
+function handleMS3DatabaseType(event) {
+    event.preventDefault();
+    let form = event.target;
+    let MS3DatabaseType = form.selectMS3DatabaseType.value;
+    console.log(MS3DatabaseType);
+}
+
 /* Event listeners */
-courseForm.addEventListener("submit", handleCourseForm)
-P4CoursePositionForm.addEventListener("submit", handle4PCoursePositionForm)
+courseForm.addEventListener("submit", handleCourseForm);
+P4CoursePositionForm.addEventListener("submit", handle4PCoursePositionForm);
+MS3DatabaseType.addEventListener("submit", handleMS3DatabaseType)
