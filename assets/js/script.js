90e8bf22059cc76c9b874d83034b36171b052811
Anna Greaves <gilhespy.anna@gmail.com>
2022-11-22 09:34:02 +0100

    Comment code.

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,10 @@ const supportBtn = document.getElementById("supportBtn");
 const resetBtn = document.getElementById("resetBtn");
 
 /**
- * Handles form to select which course the student is on
+ * Handle form to select which course the student is on.
+ * Display the next relevant dropdown menu for the selected course.
+ * Provide feedback if the student tries to submit without
+ * making a selection.
  * @param {form event} event 
  */
 function handleCourseForm(event) {
@@ -21,20 +24,23 @@ function handleCourseForm(event) {
         P5CoursePositionForm.classList.remove("d-none");
         disableCourseForm();
     } else {
-        /* Show feedback that must select an option */
         document.getElementById("courseFormFeedback").innerHTML = "Please select a course";
     }
 }
 
 
 /**
- * Handles form to select which section of the 4P course the student is on
+ * Handle form to select which section of the 4P course the student is on.
+ * Check course position selection made by student and remove d-none (display: none;) 
+ * class from the relevant checklist items in the #actionResult section of the page. 
+ * If student has worked on MS3, show form to select which DB type their project uses.
  * @param {form event} event 
  */
 function handle4PCoursePositionForm(event) {
     event.preventDefault();
     let form = event.target;
     let P4CoursePosition = form.selectP4CoursePosition.value;
+
     if (P4CoursePosition === "noSubmissions") {
         disableP4CoursePositionForm();
         showNoActionNeededResult();
@@ -48,6 +54,7 @@ function handle4PCoursePositionForm(event) {
         document.getElementById("signUpGSPWrapper").classList.remove("d-none");
         document.getElementById("signUpHerokuCreditsWrapper").classList.remove("d-none");
         document.getElementById("convertDynosWrapper").classList.remove("d-none");
+
         if (P4CoursePosition === "submittedMS3" || P4CoursePosition === "submittedMS4") {
             document.getElementById("signUpElephantSQLWrapper").classList.remove("d-none");
             document.getElementById("migrateMS4Wrapper").classList.remove("d-none");
@@ -56,11 +63,18 @@ function handle4PCoursePositionForm(event) {
         showHerokuTasksInfoText();
         document.getElementById("MS3DatabaseTypeForm").classList.remove("d-none");
     } else {
-        /* Show feedback that must select an option */
         document.getElementById("P4CoursePositionFormFeedback").innerHTML = "Please select your position in the 4P course course";
     }
 }
 
+
+/**
+ * Handle form to select which section of the 5P course the student is on.
+ * Provide feedback if student tries to submit form without selecting a valid choice.
+ * Check course position selection and remove d-none (display: none;) 
+ * class from the relevant checklist items in the #actionResult section of the page.
+ * @param {form event} event 
+ */
 function handleP5CoursePositionForm(event) {
     event.preventDefault();
     let form = event.target;
@@ -101,7 +115,8 @@ function handleP5CoursePositionForm(event) {
 
 
 /**
- * Handles form to select which type of database was used for MS3
+ * Handles form to select which type of database was used for MS3 and display the relevant checklist item
+ * for students who used a Postgres Database.
  * @param {form event} event 
  */
 function handleMS3DatabaseType(event) {
@@ -121,11 +136,12 @@ function handleMS3DatabaseType(event) {
     } else {
         document.getElementById("MS3DatabaseTypeFormFeedback").innerHTML = "Please select the type of database your MS3 uses";
     }
-
 }
 
+
 /**
- * Disable course form dropdown and its submit button.
+ * Disable course form dropdown and its submit button
+ * so if student wants to make changes they have to refresh the page to start again.
  */
 function disableCourseForm() {
     document.getElementById("selectCourse").disabled = true;
@@ -133,8 +149,10 @@ function disableCourseForm() {
     document.getElementById("courseFormFeedback").innerHTML = "";
 }
 
+
 /**
  * Disable 4P course position form dropdown and its submit button.
+ * so if student wants to make changes they have to refresh the page to start again.
  */
 function disableP4CoursePositionForm() {
     document.getElementById("selectP4CoursePosition").disabled = true;
@@ -142,18 +160,33 @@ function disableP4CoursePositionForm() {
     document.getElementById("P4CoursePositionFormFeedback").innerHTML = "";
 }
 
+
+/**
+ * Disable MS3 database type form dropdown and its submit button.
+ * so if student wants to make changes they have to refresh the page to start again.
+ */
 function disableMS3DatabaseTypeForm() {
     document.getElementById("selectMS3DatabaseType").disabled = true;
     document.getElementById("selectMS3DatabaseTypeSubmitBtn").disabled = true;
     document.getElementById("MS3DatabaseTypeFormFeedback").innerHTML = "";
 }
 
+
+/**
+ * Disable 5P course position form dropdown and its submit button.
+ * so if student wants to make changes they have to refresh the page to start again.
+ */
 function disableP5CoursePositionForm() {
     document.getElementById("selectP5CoursePosition").disabled = true;
     document.getElementById("selectP5CoursePositionSubmitBtn").disabled = true;
     document.getElementById("P5CoursePositionFormFeedback").innerHTML = "";
 }
 
+
+/**
+ * Show extra information for tasks involving Heroku 
+ * (end date and responsibility of student to ensure tasks are complete)
+ */
 function showHerokuTasksInfoText() {
     let elements = document.getElementsByClassName("herokuTasksInfoText");
     for (let element of elements) {
@@ -161,23 +194,41 @@ function showHerokuTasksInfoText() {
     }
 }
 
+
+/**
+* Show div with information for students who do not need to take any actions
+*/
 function showNoActionNeededResult() {
     document.getElementById("noActionNeeded").classList.remove("d-none");
 }
 
+
+/**
+ * Show div containing the final checklist and scroll user to the bottom of the page
+ */
 function showActionResult() {
     document.getElementById("actionResult").classList.remove("d-none");
     window.scrollTo(0,document.body.scrollHeight);
 }
 
+
+/**
+ * Show div containing the information on where to get support with the steps,
+ * and scroll user to the bottom of the page
+ */
 function showSupportInfo() {
     document.getElementById("supportInfo").classList.remove("d-none");
     window.scrollTo(0,document.body.scrollHeight);
 }
 
+
+/**
+ * Refresh the page
+ */
 function resetForm() {
     location.reload(true);
 }
+
 
 /* Event listeners */
 courseForm.addEventListener("submit", handleCourseForm);
